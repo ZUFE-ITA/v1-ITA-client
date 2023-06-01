4446c2a5e4af4bd4768c462ee705d6cb8099e090
miaobuao <miaobuao@outlook.com>
2023-06-01 19:10:45 +0800

    feat: 支持异步加载的文本组件

---
+++ src/components/AsyncText.vue
@@ -0,0 +1,18 @@
+<template>
+	<template v-if="text">
+		{{ text }}
+	</template>
+	<q-skeleton v-else type="text" height="2.5em"></q-skeleton>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	text?: string;
+}>();
+</script>
+
+<script lang="ts">
+export default {
+	name: "AsyncText",
+};
+</script>
